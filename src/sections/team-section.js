f43955668c4d53e15a1001680d7f2b24a44d90a6
Busayo Samuel <busayosamuel2016@gmail.com>
2021-08-03 13:05:36 +0100

    additional folders

---
+++ src/sections/team-section.js
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Container, Grid } from 'theme-ui';
+import SectionHeader from 'components/section-header';
+import TeamCard from 'components/team-card';
+import data from '../data/TeamData'
+
+
+export default function TeamSection() {
+  return (
+    <section>
+      <Container>
+        <SectionHeader slogan='Our Team' title='The most qualified and talented individuals'/>
+        <Grid sx={styles.grid}>
+          {data.map((item)=> (
+            <TeamCard
+            key={item.id}
+            src={item.imgSrc}
+            alt={item.altText}
+            title={item.title}
+            designation={item.designation}
+            social={item.socialProfile}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  grid: {
+    mt: [0, null, -6, null, -4],
+    gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
+    gridTemplateColumns: [
+      'repeat(2,1fr)',
+      null,
+      'repeat(2,1fr)',
+      null,
+      'repeat(3,1fr)',
+    ],
+  },
+};
